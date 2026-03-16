2adb3d2a428d9280c79b5fe8806930407403edbb
Quest-K <kjo36234311@gmail.com>
2026-03-16 17:31:05 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// [https://vite.dev/config/](https://vite.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Quest_K/', // 깃허브 페이지 배포를 위한 필수 설정입니다.
+  // 리포지토리 이름이 Quest_K 이므로 앞뒤에 /를 붙여 정확히 입력합니다.
+  base: '/Quest_K/', 
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: '0.0.0.0',
     port: 3001,
     strictPort: true,
-  },
+  }
 })
